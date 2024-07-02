394b9f573381afd01fbe42ef1e5b5dd9a44a3ff7
Harish Thampi <harishbhupeshthampi@gmail.com>
2024-07-02 18:58:37 -0400

    align about section

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -56,7 +56,7 @@ export default function Experience() {
   return (
     <div name="experience" className='bg-gradient-to-b from-gray-800 to-black
     w-full h-screen'>
-        <div className='max-w-screen-lg max-auto p-4 flex flex-col 
+        <div className='max-w-screen-lg mx-auto p-4 flex flex-col 
         justify-center w-full h-full text-white'>
             <div>
                 <p className='text-4xl font-bold border-b-4
