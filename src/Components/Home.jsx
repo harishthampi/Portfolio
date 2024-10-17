166396be0f124d19ed41db69835689f9718a6f10
Harish Thampi <harishbhupeshthampi@gmail.com>
2024-10-17 19:27:39 -0400

    Updates

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div
       name="home"
       className="min-h-screen w-full bg-gradient-to-b from-black
-     via-black to-gray-800" 
+     via-black to-gray-800 mt-10" 
     >
       <div
         className="max-w-screen-lg mx-auto flex flex-col
